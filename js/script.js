fabce4288f30e6c5cb1d91902abd18ff84313982
Robert-Stefan <robertboss05@hotmail.it>
2021-04-16 17:48:26 +0200

    Add snack 3

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ console.log('La bici col peso minore è la bici', bikes[lessWeightIndex]);*/
  * Creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
  */
 
- const randomNumber = (min, max) => {
+ /*const randomNumber = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -92,7 +92,81 @@ for(let i = 0; i < teams.length; i++) {
 };
 
 console.log('Results', teams);
-console.log('New results', newArrayResults);
+console.log('New results', newArrayResults);*/
+
+//jsnack 3 
+//Si scriva una funzione che accetti tre argomenti, un array e due numeri ( a più piccolo di b).
+//La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+
+/*const myArray = ['Paolo', 'Fabio', 'Lorenzo', 'Didier', 'Alexander', 'Salvatore'];
+let min = parseInt(prompt('Inserisci un numero tra 0 e 5'));
+while(isNaN(min) || min < 0 || min > myArray.length) {
+    min = parseInt(prompt('Inserisci un numero tra 0 e 5'));
+}
+let max = parseInt(prompt('Inserisci un numero tra '+min+' e 5'));
+while(isNaN(max) || max < min || max > myArray.length) {
+    max = parseInt(prompt('Inserisci un numero tra '+min+' e 5'));
+}
+
+//ES5
+
+let newArray = [];
+
+/*function filterArray(arr, min, max) {
+    for(var i = 0; i < arr.length; i++) {
+        const tmp = arr[i];
+        if(min <= i && max >= i) {
+            newArray.push(tmp);
+        }
+    }
+}
+
+filterArray(myArray, min, max);
+
+console.log(newArray);*/
+
+//ES6
+
+/*const newFilterArray = myArray.filter((element, index) => {
+    return min <= index && max >= index;
+})
+
+console.log(newFilterArray);*/
+
+//Con forEach
+/*function filterArray(min, max) {
+    myArray.forEach((element,index) => {
+        if(min <= index && max >= index) {
+            newArray.push(element);
+        }
+    })
+}
+
+filterArray(min, max);
+
+console.log(newArray);*/
 
 
+//New Snack
+const arrayObj = [
+    {name: 'Poppy', type: 'tshirt', color: 'red'},
+    {name: 'Jumping', type: 'occhiali', color: 'blue'},
+    {name: 'CrissCross', type: 'scarpe', color: 'black'},
+    {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
 
+const newArrayObj = arrayObj.map((element) => {
+    const newElement = {
+        ...element,
+        position: randomNumber(0,5)
+    }
+
+    return newElement;
+
+})
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+console.log(newArrayObj);
